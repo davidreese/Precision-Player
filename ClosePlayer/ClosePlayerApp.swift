1de2a4c7d640f6065a516df3da249a6dbc0a08b6
David Reese <dir2100@me.com>
2023-06-14 23:27:02 -0400

    Initial Commit

---
+++ ClosePlayer/ClosePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosePlayerApp.swift
+//  ClosePlayer
+//
+//  Created by David Reese on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ClosePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
