4004fbe74d8ec13a720f61a20506ec620d0466ec
David Reese <dir2100@me.com>
2024-08-13 08:40:21 -0400

    migrated to AVAudioPlayer

---
+++ ClosePlayer/AudioPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  AudioPlayer.swift
+//  ClosePlayer
+//
+//  Created by David Reese on 4/12/24.
+//
+
+import SwiftUI
+
+struct AudioPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AudioPlayer()
+}
---
+++ ClosePlayer/AudioPlayerModel.swift
@@ -0,0 +1,8 @@
+//
+//  AudioPlayerModel.swift
+//  ClosePlayer
+//
+//  Created by David Reese on 8/13/24.
+//
+
+import Foundation
